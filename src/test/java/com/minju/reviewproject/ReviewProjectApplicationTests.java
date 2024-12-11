ad81c35edbbad64aaafe7da315ffe28fa3ebef10
muchluvu <rorrxr@naver.com>
2024-12-11 10:29:15 +0900

    Initial commit

---
+++ src/test/java/com/minju/reviewproject/ReviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minju.reviewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
