cebb2d1d93d806bbe0a669b886954dc0102abb28
muchluvu <rorrxr@naver.com>
2024-12-13 11:23:43 +0900

    fix: dto 수정

--- src/main/java/com/minju/reviewproject/dto/ReviewRequestDto.java
+++ src/main/java/com/minju/reviewproject/dto/ReviewRequestDto.java
@@ -2,9 +2,14 @@ package com.minju.reviewproject.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class ReviewRequestDto {
+    private Long id;
     private Long userId;
     private int score;
     private String content;
+    private String imageUrl;
+    private LocalDateTime createdAt;
 }
